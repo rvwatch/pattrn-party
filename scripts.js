7dc4a32778f89b9f89c9c5b8e1024061f39c6001
Ricardo Viera <ricardoclean@gmail.com>
2018-05-23 22:29:38 -0600

    Updates existing html/css structure for the accordion section

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// $(document).ready(function () {
+  
+  
+//   $('.accordion--tabs--article').on('click', '.tab-link', function(event) {
+//     var accordionTabs = $(this).closest('.accordion--tabs--article');
+    
+//     accordionTabs.find('active').toggleClass('active');
+    
+    
+//   });
+// });
